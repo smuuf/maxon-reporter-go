fe1ea1fb836e5e007171ed4e3987383e14aba28c
Milan Dufek <dufek.milan@gmail.com>
2024-02-19 16:14:01 +0100

    Logging: Log file rotation.

--- internal/daemon.go
+++ internal/daemon.go
@@ -2,10 +2,12 @@ package internal
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
 
+	"gopkg.in/natefinch/lumberjack.v2"
 	"gopkg.in/sevlyar/go-daemon.v0"
 )
 
@@ -15,8 +17,19 @@ func Daemonize() (bool, *daemon.Context) {
 	ctx := &daemon.Context{
 		PidFileName: filepath.Join(Settings.SelfDir, ".reporter.pid"),
 		PidFilePerm: 0644,
-		LogFileName: "reporter.log",
+		LogFileName: filepath.Join(Settings.SelfDir, "reporter.log"),
+		LogFilePerm: 0644,
 	}
+
+	// Set up log rotation
+	log.SetOutput(&lumberjack.Logger{
+		Filename:   ctx.LogFileName,
+		MaxSize:    20, // MB
+		MaxBackups: 1,
+		MaxAge:     30, // days
+		Compress:   false,
+	})
+	log.Println("Log rotation enabled.")
 
 	runningProcess, _ := ctx.Search()
 	if runningProcess != nil {
